e933d459af0f2d7e5f556eee840417b83b3a6d02
Eli <iamelimars@yahoo.com>
2019-01-23 22:08:28 -0500

    Initial Commit

---
+++ contact.php
@@ -0,0 +1,243 @@
+
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>unapp Template</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+  <!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400" rel="stylesheet">
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+
+	<div class="colorlib-loader"></div>
+
+	<div id="page">
+		<nav class="colorlib-nav" role="navigation">
+			<div class="top-menu">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-2">
+							<div id="colorlib-logo"><a href="index.php">SJC.</a></div>
+						</div>
+						<div class="col-md-10 text-right menu-1">
+							<ul>
+								<li><a href="index.php">Home</a></li>
+								<li><a href="services.html">Services</a></li>
+								<li><a href="about.html">About</a></li>
+								<li class="active"><a href="contact.php">Contact</a></li>
+								<li><a target="_self" href="tel:+15616333994">561.633.3994</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+
+		<section id="home" class="video-hero" style="height: 500px; background-image: url(images/cover_img_1.jpg);  background-size:cover; background-position: center center;background-attachment:fixed;" data-section="home">
+		<div class="overlay"></div>
+			<div class="display-t display-t2 text-center">
+				<div class="display-tc display-tc2">
+					<div class="container">
+						<div class="col-md-12 col-md-offset-0">
+							<div class="animate-box">
+								<h2>Contact Us</h2>
+								<p class="breadcrumbs"><span><a href="index.html">Home</a></span> <span>Contact</span></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<div id="colorlib-contact">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 animate-box">
+						<h2>Contact Information</h2>
+						<div class="row">
+							<div class="col-md-12">
+								<div class="contact-info-wrap-flex">
+									<div class="con-info">
+										<p><span><i class="icon-location-2"></i></span> Palm Beach, Broward & Dade Counties <br> Florida</p>
+									</div>
+									<div class="con-info">
+										<p><span><i class="icon-phone3"></i></span> <a target="_self" href="tel:+15616333994">561.633.3994</a></p>
+									</div>
+									<div class="con-info">
+										<p><span><i class="icon-paperplane"></i></span> <a href="mailto:Sjc.multimediadesign@gmail.com" target="_top">Sjc.multimediadesign@gmail.com</a></p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- <div class="col-md-8 col-md-pull-4 animate-box">
+
+						<h2>Get In Touch</h2>
+						<form name="contactform" action="contact-form.php" method="post">
+							<div class="row form-group">
+								<div class="col-md-12">
+									<input type="text" id="name" class="form-control" placeholder="Your first & last name">
+								</div>
+
+							</div>
+
+							<div class="row form-group">
+								<div class="col-md-12">
+									<input type="text" id="email" class="form-control" placeholder="Your email address">
+								</div>
+							</div>
+
+							<div class="row form-group">
+								<div class="col-md-12">
+									<input type="text" id="subject" class="form-control" placeholder="Your subject of this message">
+								</div>
+							</div>
+
+							<div class="row form-group">
+								<div class="col-md-12">
+									<textarea name="message" id="message" cols="30" rows="10" class="form-control" placeholder="Say something about us"></textarea>
+								</div>
+							</div>
+							<div class="form-group">
+								<input type="submit" name="submit" value="Send Message" class="btn btn-primary">
+							</div>
+						</form>
+					</div> -->
+				</div>
+			</div>
+		</div>
+
+		<!-- <div id="map" class="colorlib-map"></div> -->
+
+		<footer id="colorlib-footer">
+			<div class="container">
+				<div class="row row-pb-md">
+					<div class="col-md-6 colorlib-widget">
+						<h4>About SJC</h4>
+						<p>Welcome to SJC Multimedia Design Home Theater Innovation Llc, we are family owned and operated company serving the South Florida Tri-county area. the leader in home integration, specializing in the installation of home theater, structured wiring, home automation, lighting control, surveillance cameras and multi-room sound system and more.</p>
+						<p>
+							<ul class="colorlib-social-icons">
+								<li><a target="_blank" href="https://twitter.com/sjc_multimedia"><i class="icon-twitter"></i></a></li>
+								<li><a target="_blank" href="https://www.facebook.com/sjc.multimedia/"><i class="icon-facebook"></i></a></li>
+								<li><a target="_blank" href="https://www.instagram.com/sjc_multimedia/"><i class="icon-instagram"></i></a></li>
+							</ul>
+						</p>
+					</div>
+					<div class="col-md-3 colorlib-widget">
+						<h4>Information</h4>
+						<p>
+							<ul class="colorlib-footer-links">
+								<li><a href="index.php"><i class="icon-check"></i> Home</a></li>
+								<li><a href="services.html"><i class="icon-check"></i> Services</a></li>
+								<li><a href="about.html"><i class="icon-check"></i> About</a></li>
+								<!-- <li><a href="#"><i class="icon-check"></i> Blog</a></li> -->
+								<li><a href="contact.php"><i class="icon-check"></i> Contact</a></li>
+								<!-- <li><a href="#"><i class="icon-check"></i> Privacy</a></li> -->
+							</ul>
+						</p>
+					</div>
+
+
+					<div class="col-md-3 colorlib-widget">
+						<h4>Contact Info</h4>
+						<ul class="colorlib-footer-links">
+							<li>Bringing great service to <br> all of South Florida</li>
+							<li><a target="_self" href="tel:+15616333994"><i class="icon-phone"></i> 561 633-3994</a></li>
+							<li><a href="mailto:Sjc.multimediadesign@gmail.com" target="_top"><i class="icon-envelope"></i>Sjc.multimediadesign@gmail.com</a></li>
+							<!-- <li><a href="#"><i class="icon-location4"></i> yourwebsite.com</a></li> -->
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="copy">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12 text-center">
+							<img class="logo-image d-sm-none d-md-block" src="images/sjc.png" alt="">
+							<!-- <p> -->
+								 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+<!-- Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a> -->
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --><br>
+								<!-- Demo Images: <a href="http://unsplash.co/" target="_blank">Unsplash</a>, <a href="http://pexels.com/" target="_blank">Pexels</a> -->
+							<!-- </p> -->
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+	</div>
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- YTPlayer -->
+	<script src="js/jquery.mb.YTPlayer.min.js"></script>
+	<!-- Owl carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Counters -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Google Map -->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCefOgb1ZWqYtj7raVSmN4PL2WkTrc-KyA&sensor=false"></script>
+	<script src="js/google_map.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
